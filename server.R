c6e9a1cfa027b64b332171440a67c4d1691c10a8
Mike <michaelalanlawson@gmail.com>
2018-03-25 12:31:50 +0100

    finished app

--- server.R
+++ server.R
@@ -1,46 +1,78 @@
-# Define server logic required to draw a histogram ----
 server <- function(input, output) {
   
-  # Histogram of the Old Faithful Geyser Data ----
-  # with requested number of bins
-  # This expression that generates a histogram is wrapped in a call
-  # to renderPlot to indicate that:
-  #
-  # 1. It is "reactive" and therefore should be automatically
-  #    re-executed when inputs (input$bins) change
-  # 2. Its output type is a plot
+  S1 <- read.csv("Spectrum1.csv")
+  S2 <- read.csv("Spectrum2.csv")
+  S3 <- read.csv("Spectrum3.csv")
+  S4 <- read.csv("Spectrum4.csv")
+  S5 <- read.csv("Spectrum5.csv")
+  SpecList <- list(S1 ,S2 ,S3 ,S4 ,S5)
+  names(SpecList) <- c("Spectrum1", "Spectrum2", "Spectrum3", "Spectrum4", "Spectrum5")
   
   output$SpectrumPlot <- renderPlotly({
     
-    S1 <- read.csv("Spectrum1.csv")
-    S2 <- read.csv("Spectrum2.csv")
-    S3 <- read.csv("Spectrum3.csv")
-    S4 <- read.csv("Spectrum4.csv")
-    S5 <- read.csv("Spectrum5.csv")
-    SpecList <- list(S1 ,S2 ,S3 ,S4 ,S5)
-    names(SpecList) <- c("Spectrum1", "Spectrum2", "Spectrum3", "Spectrum4", "Spectrum5")
-    
     BackChoice <- input$BackgroundSpectrum
+    SampChoice <- input$SampleSpectrum
     
     Background <- SpecList[[BackChoice]]
+    Sample <- SpecList[[SampChoice]]
     
-  
-      MySpectrum <- data.frame(Wavenumber = 0, Intensity = 0)
+    data <- data.frame(x = Background$Wavenumber, 
+                       Background = Background$Intensity, 
+                       Sample = Sample$Intensity)
+    
+    
       MySpectrum <- Background
       
       xaxis <- list(
-        title = "Wavenumber (cm<sup>-1</sup>)"
+        title = "Wavenumber (cm<sup>-1</sup>)",
+        range = c(700,3600)
       )
       
       yaxis <- list(
         title = "Intensity (Arb.)"
       )
       
-      plot_ly(data = MySpectrum, x = ~ Wavenumber, y= ~ Intensity, type = "scatter", mode = "lines") %>%
+      plot_ly(data = data, x = ~ x) %>%
+        add_trace(y = ~Background, name = paste("Background: ", BackChoice), mode = "lines") %>%
+        add_trace(y = ~Sample, name = paste("Sample: ", SampChoice), mode = "lines") %>%
+        
         layout(xaxis = xaxis,
                yaxis=yaxis,
-               title = "RawSpectra")
+               title = "Raw Input",
+               legend = list(orientation= "h",
+                             x = 0.2,
+                             y = 0.14))
 
+  })
+  
+  output$ProcessedPlot <- renderPlotly({
+    
+    BackChoice <- input$BackgroundSpectrum
+    SampChoice <- input$SampleSpectrum
+    
+    Background <- SpecList[[BackChoice]]
+    Sample <- SpecList[[SampChoice]]
+    
+    data <- data.frame(x = Background$Wavenumber, 
+                       Background = Background$Intensity, 
+                       Sample = Sample$Intensity)
+    
+    xaxis <- list(
+      title = "Wavenumber (cm<sup>-1</sup>)",
+      range = c(700,3600)
+    )
+    
+    yaxis <- list(
+      title = "Transmission(%)"
+    )
+    
+    plot_ly(data = data, x = ~ x) %>%
+      add_trace(y = ~Sample*100/Background, name = paste("Background: ", BackChoice), mode = "lines") %>%
+      
+      layout(xaxis = xaxis,
+             yaxis=yaxis,
+             title = "Transmission Spectrum")
+    
   })
   
 }
--- ui.R
+++ ui.R
@@ -1,26 +1,31 @@
 library(shiny)
 library(plotly)
 
-#require("plotly")
-
-
-
 ui <- fluidPage(
   
   # App title ----
-  titlePanel("Spectrum Viewer"),
+  titlePanel("Transmission Spectrum Viewer"),
   
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
     
     # Sidebar panel for inputs ----
-    sidebarPanel(
+    sidebarPanel(h4("Description", align = "left"),
+                  p("The transmission spectum viewer takes two raw input spectra,
+                   plots them in a plotly chart called \"Raw Input\" then divides the sample spectrum
+                   by the background spectrum to give the samples \"Transmission Spectrum\", shown in a second chart.
+                   "),
+                 h4("Instructions", align = "left"),
+                 p("Select from pre-stored sample and background spectra using the list inputs below.
+                   "),
+            
       
       selectInput("BackgroundSpectrum", "Select background spectrum to use:", 
-                  c("Spectrum1", "Spectrum2", "Spectrum3", "Spectrum4", "Spectrum5")),
+                  c("Spectrum1", "Spectrum2", "Spectrum3", "Spectrum4", "Spectrum5"),
+                  selected = "Spectrum2"),
       selectInput("SampleSpectrum", "Select sample spectrum to use:", 
                   c("Spectrum1", "Spectrum2", "Spectrum3", "Spectrum4", "Spectrum5"),
-                  selected = "Spectrum5")
+                  selected = "Spectrum3")
     ),
     
     
@@ -29,8 +34,9 @@ ui <- fluidPage(
       
       # Output: Histogram ----
       plotlyOutput(outputId = "SpectrumPlot"),
+      br(),
       plotlyOutput(outputId = "ProcessedPlot")
-      
+
     )
   )
 )
